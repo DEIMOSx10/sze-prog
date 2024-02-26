2f050943889dc27e6fdd58d5bca22c32b57d84f3
DEIMOSx10 <sahinrauf11@gmail.com>
2024-02-26 17:34:57 +0100

    Ati LinkedListSum (2)

--- linked-list/proba1.cpp
+++ linked-list/proba1.cpp
@@ -15,10 +15,30 @@ The function should return an array containing all values of the nodes in the li
  *The function should return the total sum of all values in the linked list.
  */
 
+/*
+*Write a function, linkedListFind, that takes in a pointer to the head of a linked list and a target value.
+*The function should return a boolean indicating whether or not the linked list contains the target.
+*/
+
+/*
+*Write a function, getNodeValue, that takes in a pointer to the head of a linked list and an index.
+*The function should return the value of the linked list at the specified index.
+If there is no node at the given index, then return -1.
+*/
 
 struct Node {
     int value;
     Node *next;
+};
+
+vector<int> linkedListValues(const Node &head) {
+    vector<int> result;
+    const Node *current = &head;
+    while (current != nullptr) {
+        result.push_back(current->value);
+        current = current->next;
+    }
+    return result;
 };
 
 int sumList(const Node *head) {
@@ -31,22 +51,35 @@ int sumList(const Node *head) {
     return sum;
 }
 
-
-vector<int> linkedListValues(const Node &head) {
-    vector<int> result;
+bool linkedListFind(const Node &head, int target) {
     const Node *current = &head;
     while (current != nullptr) {
-        result.push_back(current->value);
+        if (target == current->value) {
+            return true;
+        }
         current = current->next;
     }
-    return result;
-};
+    return false;
+}
+
+int getNodeValue(const Node &head, int index) {
+    const Node *current = &head;
+    int i = 1;
+    while (current != nullptr) {
+        if (i == index) {
+            return current->value;
+        }
+        current = current->next;
+        i++;
+    }
+    return -1;
+}
 
 int main() {
-    Node a = {1, nullptr};
-    Node b = {2, nullptr};
-    Node c = {3, nullptr};
-    Node d = {4, nullptr};
+    Node a = {54, nullptr};
+    Node b = {78, nullptr};
+    Node c = {11, nullptr};
+    Node d = {7, nullptr};
 
     a.next = &b;
     b.next = &c;
@@ -60,6 +93,18 @@ int main() {
 
     const int sum = sumList(&a);
     cout << endl << sum;
+
+    const bool contains = linkedListFind(a, 54);
+
+    if (contains) {
+        cout << endl << "It contains!";
+    } else {
+        cout << endl << "It doesn't cointain!";
+    }
+
+    int value = getNodeValue(a, 2);
+
+    cout << endl << value;
 
     return 0;
 }

c73e668d179c0b788a25311ec2867118d8aca9b7
DEIMOSx10 <sahinrauf11@gmail.com>
2024-03-05 19:28:41 +0100

    ati type gyak

---
+++ ati_gyak/gyak1.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+
+string uncompress(const string &input) {
+    string result;
+    string sszam;
+    for (int i = 0; i < input.length(); i++) {
+        if (isdigit(input[i])) {
+            sszam += input[i];
+            continue;
+        }
+
+        int szam = stoi(sszam);
+        sszam = "";
+        char betu = input[i];
+        for (int j = 0; j < szam; ++j) {
+            result += betu;
+        }
+    }
+    //0. eredmény változó (return)
+    //1. 2 karakterenként nézem a stringet
+    //minden páratlan karakter szám
+    //minden páros egy betű
+    //2. a két karakter hozzáfűzöm az eredményhez (string +=)
+    //3. return eredmény
+    return result;
+}
+
+
+
+string compress(const string &input) {
+    string result;
+
+     /*
+     *1. azonos karakterig nézem a stringet
+     *2. szamlalo letrehozasa
+     *2. elmentem, hogy hány azonos karakter volt majd tovabblepek a for ciklusban
+     *3. az adott mennyiségű karaktert hozzafuzom a resulthoz
+     */
+    int szamlalo = 1;
+    char betu = input[0];
+    for (int i = 1; i < input.length(); ++i) {
+        if (input[i] == betu) {
+            szamlalo++;
+            if (i != input.length() - 1) {
+                continue;
+            }
+        }
+
+        result += to_string(szamlalo);
+        szamlalo = 1;
+        result += betu;
+        betu = input[i];
+    }
+
+    return result;
+}
+
+int main() {
+    cout << uncompress("2c3a1t") << endl;
+    cout << uncompress("3n12e2z") << endl;
+
+    cout << compress("ccaaatsss") << endl;
+    return 0;
+}
+
--- main.cpp
+++ main.cpp
@@ -3,19 +3,6 @@
 using namespace std;
 
 
-
-
 int main() {
-
-
     return 0;
 }
-
-
-/*
- * row: 0     *
- * row: 1    **
- * row: 2   ***
- * row: 3  ****
- * row: 4 *****
- */
